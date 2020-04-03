9261844a692f228fdd3f1598fc6c57a14886b6b8
Douglas Alves Lima <douglas.felipelima18@gmail.com>
2020-04-03 10:55:12 -0300

    (#1) extracting info

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,21 @@ import System.Environment
 import qualified Network.Socket.ByteString.Lazy as Bs
 import Data.Monoid
 import Data.String
+import qualified Data.Map as M
 import JsonParser
+import qualified Data.ByteString.Lazy.Char8 as R
+import System.IO as S
+import System.Directory
+
+
+data ServerState = St {
+  port :: String,
+  rootPath :: String
+                      } 
+
+httpHeader :: String
+httpHeader = "HTTP/1.1 ok 200\n" <> "Content-Type: text/html\n"
+ 
 
 manageRequest :: String -> HttpRequest
 manageRequest xs = case httpRequest $ xs of
@@ -19,50 +33,53 @@ unsoc [] = Nothing
 unsoc [x] = Just []
 unsoc (x:xs) = (x:) <$> unsoc xs
 
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
+returnHtmlFile :: HttpRequest -> ServerState ->  IO String
+returnHtmlFile (Req {method = _, path = file})
+  (St {rootPath = x}) = do
+  putStrLn x
+  exists <- doesFileExist (x <> file)
+  if exists
+    then do input <- S.readFile $ x <> file
+            return input
+    else do input <- S.readFile $ x <> "index.html"
+            return input 
+
+
+
+mainLoop :: Socket -> ServerState -> IO ()
+mainLoop sock st = do
     (conn, addr) <- accept sock
     req <- Bs.recv conn (5000)
-    putStrLn $ "Request não tratado : ===============================\n" <> show req
     let coolReq = manageRequest $ filter (/='"') $ show req
-    putStrLn $ "Request Trado : ===============================\n" <> show coolReq
-    Bs.send conn (response coolReq)
+    htmlReq <- returnHtmlFile coolReq st
+    Bs.send conn (R.pack htmlReq)
     close conn
-    mainLoop sock
+    mainLoop sock st
 
 getFile :: [String] -> String
 getFile [] = "magic.json"
 getFile (y:_) = y
 
+
+buildState :: Json -> ServerState
+buildState (JsonObject x) =
+  St {
+  rootPath =( case M.lookup "root_path" x of
+           Just (JsonString xs) -> xs
+           Nothing -> "/var/.www" ),
+  port = case M.lookup "port" x of
+           Just (JsonString pt) -> pt
+           Nothing -> "5050"
+     }
+
 main :: IO ()
 main = do
   configfigF <- getFile <$> getArgs
-  config <- parserFile configfigF
-  addr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just "6969")
+  Right configJson <- parserFile configfigF
+  let st = buildState configJson
+  addr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just $ port st)
   sock <- socket AF_INET Stream 0
   bind sock (addrAddress addr)
   listen sock 4
-  mainLoop sock
+  mainLoop sock st
 
-response :: (Monoid m, Data.String.IsString m) => HttpRequest -> m
-response req
-  | (path req == "/") = "HTTP/1.1 200 ok\n" <>
-                        "content-type: HTML\n\n" <>
-                        "<html>" <>
-                        "<head>" <>
-                        "<title> Pure Functional Cerver </title>" <>
-                        "</head>" <>
-                        "<body>" <>
-                        "<h1> Haskell Cerver </h1>" <>
-                        "</body>" <>
-                        "</html>\n"
-  | otherwise =  "HTTP/1.1 200 ok\n" <>
-                 "content-type: HTML\n\n" <>
-                 "<html>" <>
-                 "<head>" <>
-                 "<title> Khelow  </title>" <>
-                 "</head>" <>
-                 "<body>" <>
-                 "<h1> be welcome to the Haskell Cerver </h1>" <>
-                 "</body>" <>
-                 "</html>\n"
